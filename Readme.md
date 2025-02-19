a4d09955cf64681a6f40c64510290d81a7e38fcc
jeons <etoile929@naver.com>
2025-02-19 23:50:19 +0900

    라우팅 작업

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Node.js
+
+## 환경 세팅 
+
+- `npm install ejs -s` -> ejs를 활용해서 html을 대신하기<br/>
+- `nodemon app.js` -> 자동 새로고침 <br/>
+- `express`를 사용해서 서버 만들기 <br/>
+- `express router`를 사용해서 효율적인 라우팅 관리 -> app.use를 사용해서 미들웨어 가져오기
--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-const http = require("http");
-const app = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
-    if(req.url === '/'){
-        res.end("root");
-    } else if (req.url === '/login') {
-        res.end("로그인 화면");
-    }
-});
-
-app.listen(3001, () => {
-    console.log("http");
-})
-
-
-
-// const express = require("express");
-// const app = express();
-
-// app.get("/", (req, res) => {
-//     res.send("root");
-// })
-
-// app.get("/login", (req, res) => {
-//     res.send("로그인 화면");
-// })
-
-// app.listen(3000, function (){
-//     console.log("서버가동");
+// const http = require("http");
+// const app = http.createServer((req, res) => {
+//     res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+//     if(req.url === '/'){
+//         res.end("root");
+//     } else if (req.url === '/login') {
+//         res.end("로그인 화면");
+//     }
 // });
+
+// app.listen(3001, () => {
+//     console.log("http");
+// })
+
+// 모듈
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+// 라우팅
+const home = require("./routes/home");
+
+//앱세팅 
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+
+app.use("/", home);
+
+app.listen(PORT, function (){
+    console.log("서버가동");
+});
