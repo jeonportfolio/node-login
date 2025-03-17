ac35f4d94e4f83c15c7e4aedadd2da8afb42652f
jeons <etoile929@naver.com>
2025-03-17 21:43:47 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,38 +1,45 @@
 "use strict";
 
+const { userInfo } = require("os");
+
+const fs = require("fs").promises;
+
 class UserStorage {
-    static #users = {
-        id: ["hihi", "로그인", "하이"],
-        psword: ["1234", "1234", "123456"],
-        name: ["이름1", "이름2", "이름3"]
-    };
 
-    static getUsers(...fields) {
-        const users = this.#users;
-        const newUsers = fields.reduce((newUsers, field) => {
-            if(users.hasOwnProperty(field)){
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {})
-        console.log(newUsers);
-        return newUsers;
-    }
-
-    static getUserInfo(id) {
-        const users = this.#users;
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
         const usersKeys = Object.keys(users);
         const userInfo = usersKeys.reduce((newUser, info) => {
             newUser[info] = users[info][idx];
             return newUser;
         }, {});
-        
         return userInfo;
     }
 
+    static getUsers(...fields) {
+        //const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            if(users.hasOwnProperty(field)){
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        console.log(newUsers);
+        return newUsers;
+    }
+
+    static getUserInfo(id) {
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data) => {
+            return this.#getUserInfo(data, id);
+        })
+        .catch(console.error);
+    }
+
     static save(userInfo) {
-        const users = this.#users;
+        //const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
